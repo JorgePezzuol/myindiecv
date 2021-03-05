1b6a237d5ad07759af8878dcc01c02c4b1edbdda
Jorge Pezzuol <jorge_pezzuol@hotmail.com>
2021-03-05 02:23:10 +0000

    heroku setup

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app
-  .use(express.static(path.join(__dirname, "/client/build")))
+  .use(express.static(path.join(__dirname, "client", 'build')))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  //res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile('index.html');
 });
